8651fd854d94ddb27805e3149d1979f02be7c4e7
andrewrhode <arhode2@illinois.edu>
2018-06-17 20:51:34 -0500

    InToOut is ready to be tested

--- writeShapes/src/com/jetbrains/Main.java
+++ writeShapes/src/com/jetbrains/Main.java
@@ -3,7 +3,7 @@ package com.jetbrains;
 public class Main {
     public static void main(String[] args) {
 
-        Rectangle rect = new Rectangle(0.2, 0.4, 0.005, 2, 0.07);
+        Rectangle rect = new Rectangle(0.5, 0.5, 0.005, 2, 0.07);
         System.out.println(rect.fillOutToIn());
     }
 }
--- writeShapes/src/com/jetbrains/Rectangle.java
+++ writeShapes/src/com/jetbrains/Rectangle.java
@@ -41,18 +41,13 @@ public class Rectangle extends Shape {
      * This function, and all of my fill functions will begin in the bottom left corner of the shape.
      */
     public String fillOutToIn() {
-        String gCode = "G01 " + "Z" + String.format("%.6f", getSafeZ()) + " F" + String.format("%.6f", Control.zSpeed) + "\n";
+        String gCode = "";
         double nBase = getBase() - getTip();
         double nHeight = getHeight() - getTip();
         while (nBase > 0 && nHeight > 0) {
-            gCode = gCode.concat(Control.moveRight(nBase));
-            gCode = gCode.concat(Control.moveUp(nHeight));
-            gCode = gCode.concat(Control.moveLeft(nBase));
-            gCode = gCode.concat(Control.moveDown(nHeight));
-            gCode = gCode.concat(Control.moveOut(getSafeZ()));
+            gCode = drawBox(gCode, nBase, nHeight);
             gCode = gCode.concat(Control.moveRight(getTip() * getTipScalar()));
             gCode = gCode.concat(Control.moveUp(getTip() * getTipScalar()));
-            gCode = gCode.concat(Control.moveIn(getSafeZ()));
             nBase -= getTip() * getTipScalar();
             nHeight -= getTip() * getTipScalar();
         }
@@ -62,15 +57,20 @@ public class Rectangle extends Shape {
     }
     public String fillInToOut() {
         String gCode = "";
+        //I know it doesn't make sense to declare a base or height smaller than the tip width,
+        //But I think overfilling in the middle is ok, and it's worth it to preserve the base to height ratio in the
+        //first few boxes.
+        double nBase = getTip() * (getBase() / getHeight());
+        double nHeight = getTip() * (getHeight() / getBase());
         gCode = gCode.concat(center());
-        gCode = gCode.concat(Control.moveIn(getSafeZ()));
-        gCode = gCode.concat(Control.moveOut(getSafeZ()));
-        gCode = gCode.concat(Control.moveLeft(getTip()));
-        gCode = gCode.concat(Control.moveDown(getTip()));
-        gCode = gCode.concat(Control.moveIn(getSafeZ()));
-        double currentDistHo = getTip() * (getBase() / getHeight());
-        double currentDistVert = getTip() * (getHeight() / getBase());
-
+        while (nBase < getBase() && nHeight < getHeight()) {
+            gCode = drawBox(gCode, nBase, nHeight);
+            gCode = gCode.concat(Control.moveLeft(getTip() * getTipScalar()));
+            gCode = gCode.concat(Control.moveDown(getTip() * getTipScalar()));
+            nBase += (getBase() / getHeight()) * (getTip() * getTipScalar());
+            nHeight += (getHeight() / getBase()) * (getTip() * getTipScalar());
+        }
+        gCode = cleanSpeeds(gCode);
         return gCode;
     }
     public String center() {
@@ -100,7 +100,14 @@ public class Rectangle extends Shape {
         }
         return newgCode;
     }
-    public String drawBox(final double ho, final double vert) {
+    public String drawBox(String inputgCode, final double ho, final double vert) {
+        inputgCode = inputgCode.concat(Control.moveIn(getSafeZ()));
+        inputgCode = inputgCode.concat(Control.moveRight(ho));
+        inputgCode = inputgCode.concat(Control.moveUp(vert));
+        inputgCode = inputgCode.concat(Control.moveLeft(ho));
+        inputgCode = inputgCode.concat(Control.moveDown(vert));
+        inputgCode = inputgCode.concat(Control.moveOut(getSafeZ()));
+        return inputgCode;
 
     }
 }
