bee715f8aa01658a2b73557c846fbf3611107aa0
andrewrhode <arhode2@illinois.edu>
2018-06-17 21:08:32 -0500

    Did I save this?

--- writeShapes/src/com/jetbrains/Main.java
+++ writeShapes/src/com/jetbrains/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         Rectangle rect = new Rectangle(0.5, 0.5, 0.005, 2, 0.07);
-        System.out.println(rect.fillOutToIn());
+        System.out.println(rect.fillInToOut());
     }
 }
